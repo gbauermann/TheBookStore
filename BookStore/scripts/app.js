7b81eb460b276ef52f537973a915497ff14112e8
gbauermann <gabriel.bauermann@gmail.com>
2019-04-24 23:49:57 -0300

    Add files via upload

--- BookStore/scripts/app.js
+++ BookStore/scripts/app.js
@@ -1,35 +1,93 @@
-var bookStoreApp = angular.module('bookStoreApp', []);
+var bookStoreApp = angular.module('bookStoreApp', ["ngRoute", "ngAnimate"]);
 
-bookStoreApp.controller('headerCtrl', function headerCtrl ($scope){
+bookStoreApp.config(['$routeProvider',
+function ($routeProvider){
+    $routeProvider
+    .when("/books", {
+        templateUrl: "partials/book-list.html",
+        controller: "bookListCtrl"
+    })
+    .when("/cart", {
+        templateUrl: "partials/cart-list.html",
+        controller: "cartListCtrl"
+    })
+    .otherwise( "/books")
+}]);
+
+bookStoreApp.factory("bookService", function($http){
+    return{
+        getBooks: function(termo){            
+
+            return $http({
+                method: 'GET',
+                url: 'https://www.googleapis.com/books/v1/volumes?q=' + termo + '&startIndex=0&maxResults=10',
+            })        
+        }
+    }
+});
+
+bookStoreApp.factory("cartService", function(){
+    var cart = [];
+
+    return{
+        getCart: function(){
+            return cart;
+        },
+
+        addToCart: function(book){
+            cart.push(book);
+        },
+
+        buy: function(book){
+            alert("Obrigado por comprar ", book.volumeInfo.title);
+        }
+    }
+});
+
+bookStoreApp.controller('headerCtrl', function headerCtrl ($scope, $location){
     $scope.appDetails = {
         title: "The BookStore",
         tagLine: "Mais de 1 milhão de títulos para você"
     };
+
+    $scope.nav = {};
+
+    $scope.nav.isActive = function(path){        
+            return path === $location.path();        
+    }
+
 })
 
-bookStoreApp.controller('bookListCtrl', function bookListCtrl ($scope, $http){   
+bookStoreApp.controller('cartListCtrl', function cartListCtrl ($scope, cartService){
+    $scope.cart = cartService.getCart();
+
+    $scope.buy = function(book){
+        cartService.buy(book);
+    }
+})
+
+
+bookStoreApp.controller('bookListCtrl', function bookListCtrl ($scope, $http, bookService, cartService){   
     
     $scope.doSearch = function () {
 
-        var termo = $scope.searchTerm;
-        if (termo.lenght == 0) return;
+        var termo = $scope.searchTerm;       
 
-        $scope.books = []
-
-        $http({
-            method: 'GET',
-            url: 'https://www.googleapis.com/books/v1/volumes?q=' + termo + '&startIndex=0&maxResults=10',
-        })
-        .then(function successCallback(response) {
+        var handleSuccess = function(response) {
             $scope.items = response.data.items;
-        }, function errorCallback(response) {
-            alert("Error connecting to API");
-        });
+        };
+
+        bookService.getBooks(termo).then(handleSuccess)
+
+        $scope.searchTerm = termo;
+    }
     
-    }   
+    $scope.addToCart = function(book){
+        cartService.addToCart(book);
+    }
 })
 
-angular.module('bookStoreApp').filter('cut', function () {
+bookStoreApp.filter('cut', function () {
     return function (value, wordwise, max, tail) {
         if (!value) return '';
 
